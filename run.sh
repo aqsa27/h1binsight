0bf2ededcce983581347ee54dbf4a8c665b9db6b
Aqsa Sheikh <31856178+aqsa27@users.noreply.github.com>
2018-10-30 22:14:10 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,5 +3,6 @@
 # Use this shell script to compile (if necessary) your code and then execute it. Below is an example of what might be found in this file if your program was written in Python
 #
 javac src/h1BStats.java
+javac src/H1FileReader.java
 cd src
 java h1BStats ../input/h1b_input.csv ../output/top_10_occupations.txt ../output/top_10_states.txt
